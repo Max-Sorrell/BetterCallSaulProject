122bdb2ce2266ce7cd6047a4dff15c2562369e87
Max-Sorrell <115995570+Max-Sorrell@users.noreply.github.com>
2023-05-17 19:54:07 +0100

    header working

---
+++ main.js
@@ -0,0 +1,21 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollPos != currentScrollPos) {
+    document.getElementById("header").style.top = "-100px";
+  } else {
+    document.getElementById("header").style.top = "0px";
+  }
+}
+var prevScrollPos = window.pageYOffset;
+window.onload = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollPos != currentScrollPos) {
+    document.getElementById("header").style.top = "-100px";
+  } else {
+    document.getElementById("header").style.top = "0px";
+  }
+}
+
+// exact same code as above except it means it works when the page is loaded 
